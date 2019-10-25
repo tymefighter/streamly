f329438383d00f4261dbd95e559a39ca637b1852
Pranay Sashank <pranaysashank@gmail.com>
2019-10-25 17:22:18 +0530

    Make sed gnu compatible, point packcheck to new version.

--- scripts/haddock.sh
+++ scripts/haddock.sh
@@ -2,8 +2,10 @@
 
 case "$1" in
   @*)
+    echo "$1"
     filename="${1:1}"
-    sed -i '' 's@^--odir=.*@--odir='"$TRAVIS_BUILD_DIR"'/html/@;s@^--package-version=.*@--package-version=master@;s@^--title=streamly-0.6.1@--title=streamly-master@' $filename
+    echo "$filename"
+    sed -i 's@^--odir=.*@--odir='"$TRAVIS_BUILD_DIR"'/html/@;s@^--package-version=.*@--package-version=master@;s@^--title=streamly-0.6.1@--title=streamly-master@' $filename
     haddock $1 ;;
   *) haddock $1 ;;
 esac
